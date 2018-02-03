20826d1cf22311b13986b4053e6ff14904151d93
diaominmin <diaominmin@sina.com>
2018-02-04 00:17:39 +0800

    hello-world_v0.1

---
+++ helloworld/src/dl/helloworld/helloworldActivity.java
@@ -0,0 +1,13 @@
+package dl.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class helloworldActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
